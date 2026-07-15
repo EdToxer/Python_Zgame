403811a125945c48080aca80d02503b19c40a86e
EdToxer <innokabid@gmail.com>
2026-07-15 15:12:17 +0500

    Initial commit

---
+++ code/settings.py
@@ -0,0 +1,14 @@
+from os import walk
+from os.path import join
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+TILE_SIZE = 64
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+ZOOM_LEVEL = 2.0
